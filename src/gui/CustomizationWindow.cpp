eee17120d15c29508607cb373b8f0753702300e7
Cynthia Valdes <112779720+cynthiavaldes@users.noreply.github.com>
2023-11-19 19:02:39 -0500

    user buttons CustomizationWindow.cpp update

--- src/gui/CustomizationWindow.cpp
+++ src/gui/CustomizationWindow.cpp
@@ -1,17 +1,22 @@
 #include "CustomizationWindow.h"
 #include "systray.h"
 #include "ui_CustomizationWindow.h"
+#include "addbuttonwindow.h"
+#include "personaltab.h"
 
 #include <QProcess>
 #include <QDir>
 #include <QComboBox>
 #include <QFileDialog>
 #include <QDebug>
+#include <QMessageBox>
 
 bool isFiu;
 bool isBeige;
 bool isDark;
-
+bool isBlue;
+bool isRed;
+bool isYellow;
 
 CustomizationWindow::CustomizationWindow(QWidget *parent) :
     QDialog(parent),
@@ -20,32 +25,85 @@ CustomizationWindow::CustomizationWindow(QWidget *parent) :
     ui->setupUi(this);
 }
 
-
 CustomizationWindow::~CustomizationWindow()
 {
     delete ui;
 }
 
+//Theme booleans and checks
 void CustomizationWindow::on_ThemeApplyButton_clicked()
 {
     if(ui->LoginFiuRadioButton->isChecked()){
         isFiu = true;
         isBeige = false;
         isDark = false;
-        QProcess process;
-        process.execute("echo " + QVariant(isFiu).toString());
+        isBlue = false;
+        isRed = false;
+        isYellow = false;
     } else if (ui->LoginBeigeRadioButton->isChecked()){
         isBeige = true;
         isFiu = false;
         isDark = false;
-        QProcess process;
-        process.execute("echo " + QVariant(isBeige).toString());
+        isBlue = false;
+        isRed = false;
+        isYellow = false;
     } else if(ui->LoginDarkRadioButton->isChecked()){
         isDark = true;
         isFiu = false;
         isBeige = false;
-        QProcess process;
-        process.execute("echo " + QVariant(isDark).toString());
+        isBlue = false;
+        isRed = false;
+        isYellow = false;
+    } else if(ui->LoginBlueRadioButton->isChecked()){
+        isDark = false;
+        isFiu = false;
+        isBeige = false;
+        isBlue = true;
+        isRed = false;
+        isYellow = false;
+    } else if(ui->LoginRedRadioButton->isChecked()){
+        isDark = false;
+        isFiu = false;
+        isBeige = false;
+        isBlue = false;
+        isRed = true;
+        isYellow = false;
+    } else if(ui->LoginYellowRadioButton->isChecked()){
+        isDark = false;
+        isFiu = false;
+        isBeige = false;
+        isBlue = false;
+        isRed = false;
+        isYellow = true;
+    }
+
+    if(ui->TrayThemeFiuButton->isChecked()){
+        status = 2;
+        QMessageBox::information(nullptr, tr("User Button Theme"), tr("Fiu button theme set."));
+    } else if(ui->TrayThemeDarkButton->isChecked()){
+        if(amount > 0){
+            status = 3;
+            QMessageBox::information(nullptr, tr("User Button Theme"), tr("Dark button theme set."));
+        } else {
+            QMessageBox::information(nullptr, tr("Add User Button"), tr("Please create buttons to use this theme."));
+            status = 0;
+        }
+    } else if(ui->TrayThemeBeigeButton->isChecked()){
+        if(amount > 0){
+            status = 4;
+            QMessageBox::information(nullptr, tr("User Button Theme"), tr("Beige button theme set."));
+        } else {
+            QMessageBox::information(nullptr, tr("Add User Button"), tr("Please create buttons to use this theme."));
+            status = 0;
+        }
+    } else if(ui->TrayThemeBlueButton->isChecked()){
+        if(amount > 0){
+            status = 5;
+            QMessageBox::information(nullptr, tr("User Button Theme"), tr("Blue button theme set."));
+        } else {
+            QMessageBox::information(nullptr, tr("Add User Button"), tr("Please create buttons to use this theme."));
+            status = 0;
+        }
     }
 }
 
@@ -54,12 +112,17 @@ void CustomizationWindow::on_ThemeResetButton_clicked()
     isFiu = false;
     isBeige = false;
     isDark = false;
+    isBlue = false;
+    isRed = false;
+    isYellow = false;
+    status = 0;
+    amount = 0;
+    QMessageBox::information(nullptr, tr("Customization Window"), tr("Reset all themes to default."));
 }
 
-
-
-void CustomizationWindow::on_ThemeEditButton_clicked()
+void CustomizationWindow::on_ThemeCreateButton_clicked()
 {
-
+    AddButtonWindow *window = new AddButtonWindow(static_cast<QWidget*>(QApplication::activeWindow()));
+    window->show();
 }
 
