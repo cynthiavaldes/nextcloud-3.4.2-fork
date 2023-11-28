f73fdaa538ef9f8da5e92bdbc68788b245362a37
alejandro-carballo <145369859+alejandro-carballo@users.noreply.github.com>
2023-11-28 18:31:50 -0500

    Add files via upload

--- src/gui/CustomizationWindow.cpp
+++ src/gui/CustomizationWindow.cpp
@@ -1,5 +1,6 @@
 #include "CustomizationWindow.h"
-#include "systray.h"
+#include "generalsettings.h"
+#include "personaltab.h"
 #include "ui_CustomizationWindow.h"
 #include "addbuttonwindow.h"
 #include "personaltab.h"
@@ -10,6 +11,7 @@
 #include <QFileDialog>
 #include <QDebug>
 #include <QMessageBox>
+#include <QShortcut>
 
 bool isFiu;
 bool isBeige;
@@ -17,12 +19,19 @@ bool isDark;
 bool isBlue;
 bool isRed;
 bool isYellow;
+bool themeFiu;
 
 CustomizationWindow::CustomizationWindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::CustomizationWindow)
 {
     ui->setupUi(this);
+    fillComboBox();
+    auto shortcut1 = new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_P), this); //personal tab
+    QObject::connect(shortcut1, &QShortcut::activated, this, [ this ] { openPersonalTab(); } );
+    auto shortcut2 = new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_M), this); //general settings
+    QObject::connect(shortcut2, &QShortcut::activated, this, [ this ] { openMainDialog(); } );
+    new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q), this, SLOT(close())); //close
 }
 
 CustomizationWindow::~CustomizationWindow()
@@ -33,6 +42,11 @@ CustomizationWindow::~CustomizationWindow()
 //Theme booleans and checks
 void CustomizationWindow::on_ThemeApplyButton_clicked()
 {
+    if(ui->GeneralThemeComboBox->currentText() == "fiu"){
+        themeFiu = true;
+    } else {
+        themeFiu = false;
+    }
     if(ui->LoginFiuRadioButton->isChecked()){
         isFiu = true;
         isBeige = false;
@@ -126,3 +140,27 @@ void CustomizationWindow::on_ThemeCreateButton_clicked()
     window->show();
 }
 
+void CustomizationWindow::fillComboBox()
+{
+    if(themeFiu == true){
+        ui->GeneralThemeComboBox->addItem("fiu");
+        ui->GeneralThemeComboBox->addItem("default");
+    } else {
+        ui->GeneralThemeComboBox->addItem("default");
+        ui->GeneralThemeComboBox->addItem("fiu");
+    }
+}
+
+void CustomizationWindow::openMainDialog()
+{
+    close();
+    OCC::GeneralSettings *window = new OCC::GeneralSettings(static_cast<QWidget*>(QApplication::activeWindow()));
+    window->show();
+}
+
+void CustomizationWindow::openPersonalTab()
+{
+    close();
+    PersonalTab *window = new PersonalTab(static_cast<QWidget*>(QApplication::activeWindow()));
+    window->show();
+}
